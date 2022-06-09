13e71a1363eb1e8fa6d91720d3f987f52cbf54bd
samodra33 <gregoriusteguh@gmail.com>
2022-06-09 15:04:27 +0700

    login page / web route

---
+++ resources/views/dashboard.php
@@ -0,0 +1,84 @@
+@extends('layout.main')
+@section('content')
+
+@if(session()->has('not_permitted'))
+  <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('not_permitted') }}</div>
+@endif
+@if(session()->has('message'))
+  <div class="alert alert-success alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('message') }}</div>
+@endif
+      <div class="row">
+        <div class="container-fluid">
+          <div class="col-md-12">
+            <div class="brand-text float-left mt-4">
+                <h3>{{trans('file.welcome')}} <span>{{Auth::user()->name}}</span> </h3>
+            </div>
+            @if(in_array("revenue_profit_summary", $all_permission))
+            <div class="filter-toggle btn-group">
+              <button class="btn btn-secondary date-btn" data-start_date="{{date('Y-m-d')}}" data-end_date="{{date('Y-m-d')}}">{{trans('file.Today')}}</button>
+              <button class="btn btn-secondary date-btn" data-start_date="{{date('Y-m-d', strtotime(' -7 day'))}}" data-end_date="{{date('Y-m-d')}}">{{trans('file.Last 7 Days')}}</button>
+              <button class="btn btn-secondary date-btn active" data-start_date="{{date('Y').'-'.date('m').'-'.'01'}}" data-end_date="{{date('Y-m-d')}}">{{trans('file.This Month')}}</button>
+              <button class="btn btn-secondary date-btn" data-start_date="{{date('Y').'-01'.'-01'}}" data-end_date="{{date('Y').'-12'.'-31'}}">{{trans('file.This Year')}}</button>
+            </div>
+            @endif
+          </div>
+        </div>
+      </div>
+      <!-- Counts Section -->
+      <section class="dashboard-counts">
+        <div class="container-fluid">
+          <div class="row">
+</div>
+        </div>
+      </section>
+
+
+@endsection
+
+@push('scripts')
+<script type="text/javascript">
+    // Show and hide color-switcher
+    $(".color-switcher .switcher-button").on('click', function() {
+        $(".color-switcher").toggleClass("show-color-switcher", "hide-color-switcher", 300);
+    });
+
+    // Color Skins
+    $('a.color').on('click', function() {
+        /*var title = $(this).attr('title');
+        $('#style-colors').attr('href', 'css/skin-' + title + '.css');
+        return false;*/
+        $.get('setting/general_setting/change-theme/' + $(this).data('color'), function(data) {
+        });
+        var style_link= $('#custom-style').attr('href').replace(/([^-]*)$/, $(this).data('color') );
+        $('#custom-style').attr('href', style_link);
+    });
+
+    $(".date-btn").on("click", function() {
+        $(".date-btn").removeClass("active");
+        $(this).addClass("active");
+        var start_date = $(this).data('start_date');
+        var end_date = $(this).data('end_date');
+        $.get('dashboard-filter/' + start_date + '/' + end_date, function(data) {
+            dashboardFilter(data);
+        });
+    });
+
+    function dashboardFilter(data){
+        $('.revenue-data').hide();
+        $('.revenue-data').html(parseFloat(data[0]).toFixed(2));
+        $('.revenue-data').show(500);
+
+        $('.return-data').hide();
+        $('.return-data').html(parseFloat(data[1]).toFixed(2));
+        $('.return-data').show(500);
+
+        $('.profit-data').hide();
+        $('.profit-data').html(parseFloat(data[2]).toFixed(2));
+        $('.profit-data').show(500);
+
+        $('.purchase_return-data').hide();
+        $('.purchase_return-data').html(parseFloat(data[3]).toFixed(2));
+        $('.purchase_return-data').show(500);
+    }
+</script>
+@endpush
