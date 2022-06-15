6e5aaa73a45b2a64210ae25c578a4e2406b32353
Pajar <pajar@viztech.com.sg>
2022-06-15 15:41:41 +0700

    Add 2FA Reset in user profile page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,21 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\User;
-use App\Roles;
 use App\Biller;
-use App\Warehouse;
-use App\CustomerGroup;
 use App\Customer;
+use App\CustomerGroup;
+use App\Roles;
+use App\User;
+use App\Warehouse;
 use Auth;
 use Hash;
-use Keygen;
-use Illuminate\Validation\Rule;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
-use App\Mail\UserNotification;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Validation\Rule;
+use Keygen;
+use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
@@ -24,29 +22,33 @@ class UserController extends Controller
     public function index()
     {
         $role = Role::find(Auth::user()->role_id);
-        if($role->hasPermissionTo('users-index')){
+        if ($role->hasPermissionTo('users-index')) {
             $permissions = Role::findByName($role->name)->permissions;
-            foreach ($permissions as $permission)
+            foreach ($permissions as $permission) {
                 $all_permission[] = $permission->name;
+            }
+
             $lims_user_list = User::where('is_deleted', false)->get();
             return view('user.index', compact('lims_user_list', 'all_permission'));
-        }
-        else
+        } else {
             return redirect()->back()->with('not_permitted', 'Sorry! You are not allowed to access this module');
+        }
+
     }
 
     public function create()
     {
         $role = Role::find(Auth::user()->role_id);
-        if($role->hasPermissionTo('users-add')){
+        if ($role->hasPermissionTo('users-add')) {
             $lims_role_list = Roles::where('is_active', true)->get();
             $lims_biller_list = Biller::where('is_active', true)->get();
             $lims_warehouse_list = Warehouse::where('is_active', true)->get();
             $lims_customer_group_list = CustomerGroup::where('is_active', true)->get();
             return view('user.create', compact('lims_role_list', 'lims_biller_list', 'lims_warehouse_list', 'lims_customer_group_list'));
-        }
-        else
+        } else {
             return redirect()->back()->with('not_permitted', 'Sorry! You are not allowed to access this module');
+        }
+
     }
 
     public function generatePassword()
@@ -60,24 +62,24 @@ class UserController extends Controller
         $this->validate($request, [
             'name' => [
                 'max:255',
-                    Rule::unique('users')->where(function ($query) {
+                Rule::unique('users')->where(function ($query) {
                     return $query->where('is_deleted', false);
                 }),
             ],
             'email' => [
                 'email',
                 'max:255',
-                    Rule::unique('users')->where(function ($query) {
+                Rule::unique('users')->where(function ($query) {
                     return $query->where('is_deleted', false);
                 }),
             ],
         ]);
-        
-        if($request->role_id == 5) {
+
+        if ($request->role_id == 5) {
             $this->validate($request, [
                 'phone_number' => [
                     'max:255',
-                        Rule::unique('customers')->where(function ($query) {
+                    Rule::unique('customers')->where(function ($query) {
                         return $query->where('is_active', 1);
                     }),
                 ],
@@ -87,47 +89,49 @@ class UserController extends Controller
         $data = $request->all();
         $message = 'User created successfully';
         try {
-            Mail::send( 'mail.user_details', $data, function( $message ) use ($data)
-            {
-                $message->to( $data['email'] )->subject( 'User Account Details' );
+            Mail::send('mail.user_details', $data, function ($message) use ($data) {
+                $message->to($data['email'])->subject('User Account Details');
             });
-        }
-        catch(\Exception $e){
+        } catch (\Exception $e) {
             $message = 'User created successfully. Please setup your <a href="setting/mail_setting">mail setting</a> to send mail.';
         }
-        if(!isset($data['is_active']))
+        if (!isset($data['is_active'])) {
             $data['is_active'] = false;
+        }
+
         $data['is_deleted'] = false;
         $data['password'] = bcrypt($data['password']);
         $data['phone'] = $data['phone_number'];
         User::create($data);
-        if($data['role_id'] == 5) {
+        if ($data['role_id'] == 5) {
             $data['name'] = $data['customer_name'];
             $data['phone_number'] = $data['phone'];
             $data['is_active'] = true;
             Customer::create($data);
         }
-        return redirect('user')->with('message1', $message); 
+        return redirect('user')->with('message1', $message);
     }
 
     public function edit($id)
     {
         $role = Role::find(Auth::user()->role_id);
-        if($role->hasPermissionTo('users-edit')){
+        if ($role->hasPermissionTo('users-edit')) {
             $lims_user_data = User::find($id);
             $lims_role_list = Roles::where('is_active', true)->get();
             $lims_biller_list = Biller::where('is_active', true)->get();
             $lims_warehouse_list = Warehouse::where('is_active', true)->get();
             return view('user.edit', compact('lims_user_data', 'lims_role_list', 'lims_biller_list', 'lims_warehouse_list'));
-        }
-        else
+        } else {
             return redirect()->back()->with('not_permitted', 'Sorry! You are not allowed to access this module');
+        }
+
     }
 
     public function update(Request $request, $id)
     {
-        if(!env('USER_VERIFIED'))
+        if (!env('USER_VERIFIED')) {
             return redirect()->back()->with('not_permitted', 'This feature is disable for demo!');
+        }
 
         $this->validate($request, [
             'name' => [
@@ -139,17 +143,21 @@ class UserController extends Controller
             'email' => [
                 'email',
                 'max:255',
-                    Rule::unique('users')->ignore($id)->where(function ($query) {
+                Rule::unique('users')->ignore($id)->where(function ($query) {
                     return $query->where('is_deleted', false);
                 }),
             ],
         ]);
 
         $input = $request->except('password');
-        if(!isset($input['is_active']))
+        if (!isset($input['is_active'])) {
             $input['is_active'] = false;
-        if(!empty($request['password']))
+        }
+
+        if (!empty($request['password'])) {
             $input['password'] = bcrypt($request['password']);
+        }
+
         $lims_user_data = User::find($id);
         $lims_user_data->update($input);
         return redirect('user')->with('message2', 'Data updated successfullly');
@@ -163,8 +171,9 @@ class UserController extends Controller
 
     public function profileUpdate(Request $request, $id)
     {
-        if(!env('USER_VERIFIED'))
+        if (!env('USER_VERIFIED')) {
             return redirect()->back()->with('not_permitted', 'This feature is disable for demo!');
+        }
 
         $input = $request->all();
         $lims_user_data = User::find($id);
@@ -174,23 +183,36 @@ class UserController extends Controller
 
     public function changePassword(Request $request, $id)
     {
-        if(!env('USER_VERIFIED'))
+        if (!env('USER_VERIFIED')) {
             return redirect()->back()->with('not_permitted', 'This feature is disable for demo!');
+        }
 
         $input = $request->all();
         $lims_user_data = User::find($id);
-        if($input['new_pass'] != $input['confirm_pass'])
-            return redirect("user/" .  "profile/" . $id )->with('message2', "Please Confirm your new password");
+        if ($input['new_pass'] != $input['confirm_pass']) {
+            return redirect("user/" . "profile/" . $id)->with('message2', "Please Confirm your new password");
+        }
 
         if (Hash::check($input['current_pass'], $lims_user_data->password)) {
             $lims_user_data->password = bcrypt($input['new_pass']);
             $lims_user_data->save();
-        }
-        else {
-            return redirect("user/" .  "profile/" . $id )->with('message1', "Current Password doesn't match");
+        } else {
+            return redirect("user/" . "profile/" . $id)->with('message1', "Current Password doesn't match");
         }
         auth()->logout();
         return redirect('/');
+    }
+
+    public function twoFAReset(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->google2fa_secret = null;
+        $user->save();
+
+        Auth::logout();
+        $request->session()->flush();
+
+        return redirect()->route('login');
     }
 
     public function deleteBySelection(Request $request)
@@ -207,18 +229,20 @@ class UserController extends Controller
 
     public function destroy($id)
     {
-        if(!env('USER_VERIFIED'))
+        if (!env('USER_VERIFIED')) {
             return redirect()->back()->with('not_permitted', 'This feature is disable for demo!');
-        
+        }
+
         $lims_user_data = User::find($id);
         $lims_user_data->is_deleted = true;
         $lims_user_data->is_active = false;
         $lims_user_data->save();
-        if(Auth::id() == $id){
+        if (Auth::id() == $id) {
             auth()->logout();
             return redirect('/login');
-        }
-        else
+        } else {
             return redirect('user')->with('message3', 'Data deleted successfullly');
+        }
+
     }
 }
