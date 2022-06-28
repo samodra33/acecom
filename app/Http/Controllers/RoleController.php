2e96cf57d7006a363aa8a53e74e2ab869e5d7d92
samodra33 <gregoriusteguh@gmail.com>
2022-06-28 16:48:57 +0700

    purchase permissions demo remove

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -88,8 +88,6 @@ class RoleController extends Controller
 
     public function setPermission(Request $request)
     {
-        if(!env('USER_VERIFIED'))
-            return redirect()->back()->with('not_permitted', 'This feature is disable for demo!');
 
         $role = Role::firstOrCreate(['id' => $request['role_id']]);
 
@@ -1105,8 +1103,6 @@ class RoleController extends Controller
 
     public function destroy($id)
     {
-        if(!env('USER_VERIFIED'))
-            return redirect()->back()->with('not_permitted', 'This feature is disable for demo!');
         $lims_role_data = Roles::find($id);
         $lims_role_data->is_active = false;
         $lims_role_data->save();
